70a878fe3ecda341210287c171a287229617bf2c
ken <ken_ore@hotmail.com>
2020-10-22 14:18:23 -0500

    problem2 passing leetcode

--- problem2-auto-complete-system.js
+++ problem2-auto-complete-system.js
@@ -1,185 +1,85 @@
 //https://leetcode.com/problems/design-search-autocomplete-system/
+//runtime: input method: O(nlgn)
+//storage: O(n) nodes in the priority queue
+//passing leetcode?: yes
+//obstacles: lexical comparison in javascript was different than in the library i used (true / false vs -1, 0, 1)
+//explanation:
+//  initialize a hashmap of the initial sentences
+//  for each keyboard input
+//    if it is a #, update or create entry in hashmap and return
+//
+//  for each sentence
+//     if it startsWith(current string) add to a priority queue
+//  pop top 3 entries from priority queue
+//
+//  Note: priority queue insertion will be based first on count, then on lexicographic order
 
-// from https://reactgo.com/javascript-heap-datastructure/
-class  BinaryHeap{
-  constructor(){
-    // this.heap = [30, 20, 10, 7, 9, 5]
-    this.heap = []
+// priorityqueue from https://stackoverflow.com/questions/42919469/efficient-way-to-implement-priority-queue-in-javascript
+const mytop = 0,
+  myparent = (c) => ((c + 1) >>> 1) - 1,
+  myleft = (c) => (c << 1) + 1,
+  myright = (c) => (c + 1) << 1;
+class PriorityQueue {
+  constructor(c = (d, e) => d > e) {
+    (this._heap = []), (this._comparator = c);
   }
-
-  insert(value){
-
-    this.heap.push(value)
-    this.bubbleUp();
+  size() {
+    return this._heap.length;
   }
-
-  //using iterative approach
-  bubbleUp(){
-    let index =  this.heap.length-1;
-
-    while( index > 0){
-      let element = this.heap[index],
-        parentIndex = Math.floor((index-1)/2),
-        parent = this.heap[parentIndex];
-
-      if(parent >= element) break
-      this.heap[index] = parent;
-      this.heap[parentIndex] = element;
-      index = parentIndex
-
-    }
+  isEmpty() {
+    return 0 == this.size();
   }
-
-  extractMax(){
-    const max = this.heap[0];
-    this.heap[0]= this.heap.pop()
-    this.sinkDown(0)
-    return max
+  peek() {
+    return this._heap[mytop];
   }
-
-  sinkDown(index){
-
-    let   left = 2*index+1,
-      right = 2*index+2,
-      largest = index;
-    const length = this.heap.length
-
-
-
-    if(left <= length &&  this.heap[left]>this.heap[largest] ){
-      largest = left
-    }
-    if(right <= length && this.heap[right]>this.heap[largest]) {
-      largest = right
-    }
-    // swap
-    if(largest !== index){
-      [this.heap[largest],this.heap[index]] = [this.heap[index],this.heap[largest]]
-      this.sinkDown(largest)
-    }
+  push(...c) {
+    return (
+      c.forEach((d) => {
+        this._heap.push(d), this._siftUp();
+      }),
+        this.size()
+    );
   }
-
-}
-
-// from https://eloquentjavascript.net/1st_edition/appendix2.html
-function BinaryHeap(scoreFunction){
-  this.content = [];
-  this.scoreFunction = scoreFunction;
-}
-
-BinaryHeap.prototype = {
-  push: function(element) {
-    // Add the new element to the end of the array.
-    this.content.push(element);
-    // Allow it to bubble up.
-    this.bubbleUp(this.content.length - 1);
-  },
-
-  pop: function() {
-    // Store the first element so we can return it later.
-    var result = this.content[0];
-    // Get the element at the end of the array.
-    var end = this.content.pop();
-    // If there are any elements left, put the end element at the
-    // start, and let it sink down.
-    if (this.content.length > 0) {
-      this.content[0] = end;
-      this.sinkDown(0);
-    }
-    return result;
-  },
-
-  remove: function(node) {
-    var length = this.content.length;
-    // To remove a value, we must search through the array to find
-    // it.
-    for (var i = 0; i < length; i++) {
-      if (this.content[i] != node) continue;
-      // When it is found, the process seen in 'pop' is repeated
-      // to fill up the hole.
-      var end = this.content.pop();
-      // If the element we popped was the one we needed to remove,
-      // we're done.
-      if (i == length - 1) break;
-      // Otherwise, we replace the removed element with the popped
-      // one, and allow it to float up or sink down as appropriate.
-      this.content[i] = end;
-      this.bubbleUp(i);
-      this.sinkDown(i);
-      break;
-    }
-  },
-
-  size: function() {
-    return this.content.length;
-  },
-
-  bubbleUp: function(n) {
-    // Fetch the element that has to be moved.
-    var element = this.content[n], score = this.scoreFunction(element);
-    // When at 0, an element can not go up any further.
-    while (n > 0) {
-      // Compute the parent element's index, and fetch it.
-      var parentN = Math.floor((n + 1) / 2) - 1,
-        parent = this.content[parentN];
-      // If the parent has a lesser score, things are in order and we
-      // are done.
-      if (score >= this.scoreFunction(parent))
-        break;
-
-      // Otherwise, swap the parent with the current element and
-      // continue.
-      this.content[parentN] = element;
-      this.content[n] = parent;
-      n = parentN;
-    }
-  },
-
-  sinkDown: function(n) {
-    // Look up the target element and its score.
-    var length = this.content.length,
-      element = this.content[n],
-      elemScore = this.scoreFunction(element);
-
-    while(true) {
-      // Compute the indices of the child elements.
-      var child2N = (n + 1) * 2, child1N = child2N - 1;
-      // This is used to store the new position of the element,
-      // if any.
-      var swap = null;
-      // If the first child exists (is inside the array)...
-      if (child1N < length) {
-        // Look it up and compute its score.
-        var child1 = this.content[child1N],
-          child1Score = this.scoreFunction(child1);
-        // If the score is less than our element's, we need to swap.
-        if (child1Score < elemScore)
-          swap = child1N;
-      }
-      // Do the same checks for the other child.
-      if (child2N < length) {
-        var child2 = this.content[child2N],
-          child2Score = this.scoreFunction(child2);
-        if (child2Score < (swap == null ? elemScore : child1Score))
-          swap = child2N;
-      }
-
-      // No need to swap further, we are done.
-      if (swap == null) break;
-
-      // Otherwise, swap and continue.
-      this.content[n] = this.content[swap];
-      this.content[swap] = element;
-      n = swap;
-    }
+  pop() {
+    const c = this.peek(),
+      d = this.size() - 1;
+    return d > mytop && this._swap(mytop, d), this._heap.pop(), this._siftDown(), c;
+  }
+  replace(c) {
+    const d = this.peek();
+    return (this._heap[mytop] = c), this._siftDown(), d;
+  }
+  _greater(c, d) {
+    return this._comparator(this._heap[c], this._heap[d]);
+  }
+  _swap(c, d) {
+    [this._heap[c], this._heap[d]] = [this._heap[d], this._heap[c]];
+  }
+  _siftUp() {
+    for (let c = this.size() - 1; c > mytop && this._greater(c, myparent(c)); ) this._swap(c, myparent(c)), (c = myparent(c));
+  }
+  _siftDown() {
+    for (let d, c = mytop; (myleft(c) < this.size() && this._greater(myleft(c), c)) || (myright(c) < this.size() && this._greater(myright(c), c)); )
+      (d = myright(c) < this.size() && this._greater(myright(c), myleft(c)) ? myright(c) : myleft(c)), this._swap(c, d), (c = d);
   }
 }
+
 /**
  * @param {string[]} sentences
  * @param {number[]} times
  */
 var AutocompleteSystem = function(sentences, times) {
+  let sentencesMap = new Map()
 
+  sentences.forEach((sentence, index) => {
+    sentencesMap.set(sentence, times[index])
+  })
+
+  return Object.assign(
+    Object.create(AutocompleteSystem.prototype), {
+      sentencesMap, input_str: ''
+    }
+  )
 };
 
 /**
@@ -187,7 +87,35 @@ var AutocompleteSystem = function(sentences, times) {
  * @return {string[]}
  */
 AutocompleteSystem.prototype.input = function(c) {
+  if (c === '#') {
+    if (this.input_str.length > 0) {
+      let curCount = this.sentencesMap.get(this.input_str) == null ? 0 : this.sentencesMap.get(this.input_str)
+      this.sentencesMap.set(this.input_str, curCount + 1)
+      this.input_str = ''
+    }
+    return []
+  }
+  this.input_str += c
 
+  let priorityQueue = new PriorityQueue((a, b) => {
+    if (a.count !== b.count) {
+      return a.count > b.count
+    }
+    return a.sentence.localeCompare(b.sentence) == -1
+  })
+
+  this.sentencesMap.forEach((value, key) => {
+    if (key.startsWith(this.input_str)) {
+      priorityQueue.push({count: value, sentence: key})
+    }
+  })
+
+  let output = []
+  while (!priorityQueue.isEmpty() && output.length < 3) {
+    output.push(priorityQueue.pop().sentence)
+  }
+
+  return output;
 };
 
 /**
@@ -195,3 +123,13 @@ AutocompleteSystem.prototype.input = function(c) {
  * var obj = new AutocompleteSystem(sentences, times)
  * var param_1 = obj.input(c)
  */
+let macs = new AutocompleteSystem(["i love you","island","iroman","i love leetcode"],[5,3,2,2])
+let result;
+result = macs.input('i')
+result = macs.input(' ')
+result = macs.input('a')
+result = macs.input('#')
+
+
+// ["AutocompleteSystem","input","input","input","input","input","input","input","input","input","input","input","input"]
+// [[["i love you","island","iroman","i love leetcode"],[5,3,2,2]],["i"],[" "],["a"],["#"],["i"],[" "],["a"],["#"],["i"],[" "],["a"],["#"]]
