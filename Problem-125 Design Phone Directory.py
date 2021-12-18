9d2d4b0d6e4317f9b0deeec3cad56dfe68f18dbf
Adhish Shrivastava <adhish70@gmail.com>
2021-12-18 12:31:25 -0500

    Initial commit

---
+++ Problem-125 Design Phone Directory.py
@@ -0,0 +1,20 @@
+# 379. Design Phone Directory
+# https://leetcode.com/problems/design-phone-directory/
+
+# Logic: 
+
+# Time Complexity:
+# Space Complexity:
+
+class PhoneDirectory:
+    def __init__(self, maxNumbers: int):
+        pass
+    
+    def get(self) -> int:
+        pass
+
+    def check(self, number: int) -> bool:     
+        pass
+
+    def release(self, number: int) -> None:
+        pass
---
+++ Problem-126 Design Search Autocomplete System.py
@@ -0,0 +1,20 @@
+# 642. Design Search Autocomplete System
+# https://leetcode.com/problems/design-search-autocomplete-system/
+
+# Logic: 
+
+# Time Complexity:
+# Space Complexity:
+
+class AutocompleteSystem:
+
+    def __init__(self, sentences: List[str], times: List[int]):
+        pass
+
+    def input(self, c: str) -> List[str]:
+        pass
+
+
+# Your AutocompleteSystem object will be instantiated and called as such:
+# obj = AutocompleteSystem(sentences, times)
+# param_1 = obj.input(c)
