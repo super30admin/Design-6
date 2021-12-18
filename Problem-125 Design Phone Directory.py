4e7e523da1d725cf2d75dad81af535785a2376a7
Adhish Shrivastava <adhish70@gmail.com>
2021-12-18 12:32:38 -0500

    Problem 125 done

--- Problem-125 Design Phone Directory.py
+++ Problem-125 Design Phone Directory.py
@@ -1,20 +1,39 @@
 # 379. Design Phone Directory
 # https://leetcode.com/problems/design-phone-directory/
 
-# Logic: 
+# Space Complexity: O(n)
 
-# Time Complexity:
-# Space Complexity:
-
+from collections import deque
 class PhoneDirectory:
     def __init__(self, maxNumbers: int):
-        pass
-    
-    def get(self) -> int:
-        pass
+        self.new = deque()
+        self.used = set()
+        for i in range(maxNumbers):
+            self.new.append(i)
 
-    def check(self, number: int) -> bool:     
-        pass
+    def get(self) -> int:
+    # Time Complexity: O(1)
+        if len(self.new) > 0:
+            num = self.new.popleft()
+            self.used.add(num)
+            return num
+        return -1
+
+    def check(self, number: int) -> bool:
+    # Time Complexity: O(1)
+        if number not in self.used:
+            return True
+        return False        
 
     def release(self, number: int) -> None:
-        pass
+    # Time Complexity: O(1)
+        if number in self.used:
+            self.used.remove(number)
+            self.new.append(number)
+        
+
+# Your PhoneDirectory object will be instantiated and called as such:
+# obj = PhoneDirectory(maxNumbers)
+# param_1 = obj.get()
+# param_2 = obj.check(number)
+# obj.release(number)
