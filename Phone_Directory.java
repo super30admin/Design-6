84285b3ad9ed1006b969b4ceb593de5cace5b0c1
Spandana Edupuganti <spandanaedupuganti31@gmail.com>
2021-12-22 21:09:16 -0500

    Design-6 completed

---
+++ Phone_Directory.java
@@ -0,0 +1,29 @@
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int[] result=new int[temperatures.length];
+        Stack<Integer> stack=new Stack<>();
+        stack.push(0);
+        for(int i=1;i<temperatures.length;i++){
+       
+            while(!stack.isEmpty() && temperatures[i]>temperatures[stack.peek()]){
+               int top=stack.peek();
+                result[top]=i-top;
+                stack.pop();
+            }
+            
+             stack.push(i);
+        }
+        
+        while(!stack.isEmpty()){
+            int i=stack.pop();
+            result[i]=0;
+        }
+        
+        return result;
+    }
+}
