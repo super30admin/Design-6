7dbeaac8b8498fda251425e79357032c8a5b29b6
Renali01 <59395990+Renali01@users.noreply.github.com>
2022-05-31 23:38:11 -0400

    Done Design 6

---
+++ PhoneDirectory.py
@@ -0,0 +1,14 @@
+# O(1) time
+# O(n) space
+
+def __init__(self, maxNumbers):
+   self.available = set(range(maxNumbers))
+
+def get(self):
+   return self.available.pop() if self.available else -1
+
+def check(self, number):
+   return number in self.available
+
+def release(self, number):
+   self.available.add(number)
