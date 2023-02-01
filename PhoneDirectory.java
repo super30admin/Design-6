88aebb32d3a60947e4b44753c24ee770e820e57a
ManishPandey1508 <manish.123kumarpandey@gmail.com>
2023-01-31 20:38:11 -0800

    Completed Design 6, please review

---
+++ PhoneDirectory.java
@@ -0,0 +1,42 @@
+import java.util.HashSet;
+import java.util.Queue;
+import java.util.Set;
+import java.util.LinkedList;
+// Time : O(1) each method is constant time  
+
+public class PhoneDirectory {
+	 Set<Integer> set;
+	    Queue<Integer> queue;
+
+	    public PhoneDirectory(int maxNumbers) {
+	        set = new HashSet<>();
+	        queue = new LinkedList<>();
+	        for(int i=0;i<maxNumbers;i++){
+	            set.add(i);
+	            queue.add(i);
+	        }
+	    }
+	    
+	    public int get() {
+	        if(queue.isEmpty())
+	            return -1;
+	        else{
+	            int num = queue.poll();
+	            set.remove(num);  
+	            return num;  
+	        }
+	    }
+	    
+	    public boolean check(int number) {
+	        return set.contains(number);
+	    }
+	    
+	    public void release(int number) {
+	        if(!set.contains(number))
+	        { 
+	        queue.add(number);
+	        set.add(number);
+	        }
+	    
+	    }
+}
