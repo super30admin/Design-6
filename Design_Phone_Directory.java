8e817e8b95b254a4c9fa22fb49bb8941f7e641db
sharvanitamaskar <sharvanitamaskar@gmail.com>
2023-10-13 11:59:20 -0400

    completed Design-6

---
+++ Design_Phone_Directory.java
@@ -0,0 +1,19 @@
+class PhoneDirectory {
+    Set<Integer> set;
+    public PhoneDirectory(int maxNumbers) {
+        set= new HashSet<Integer>();
+        for(int i=0; i<maxNumbers; i++)set.add(i);
+    }
+    public int get() {
+        if(set.size()==0)return -1;
+        int num = set.iterator().next();
+        set.remove(num);
+        return num;
+    }
+    public boolean check(int number) {
+        return set.contains(number);
+    }
+    public void release(int number) {
+        set.add(number);
+    }
+}
